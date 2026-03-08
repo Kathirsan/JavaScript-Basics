f74093917f75b0bb11ae59deb8004f53ba8ecee8
Kathirsan <vivekanantharajakathirsan2003@gmail.com>
2026-03-08 09:28:07 +0530

    upload

--- type.js
+++ type.js
@@ -1,2 +1,12 @@
 const hero=(name)=>console.log(`favourite hero ${name} like`);
-hero("rajini")
+hero("rajini");
+var gandhiji="father of nation";
+console.log(gandhiji);
+function starbucks(){
+    console.log("waiter");
+    user()
+}
+starbucks();
+function user(){
+    console.log("customer")
+}
